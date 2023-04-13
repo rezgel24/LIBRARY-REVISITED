4b64b1457cd03ce22934257021087336f843551a
rezgel24 <angelolim2412@gmail.com>
2023-04-13 21:05:10 +0800

    Added more to Initial Codde

--- js/script.js
+++ js/script.js
@@ -5,12 +5,25 @@ const addBtn = document.getElementById("add-book-btn");
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
+
 const library = [];
 
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+}
+
+function addNewBook(event) {
+  event.preventDefault();
+
+  if (title.value === "" || author.value === "" || pages.value === "") return;
+
+  const book = new Book(title.value, author.value, pages.value);
+  library.push(book);
+
+  clearInputs();
+  closeModal();
 }
 
 function clearInputs() {
@@ -27,16 +40,6 @@ function openModal() {
   modal.style.display = "flex";
 }
 
-addBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  const book = new Book(title.value, author.value, pages.value);
-
-  library.push(book);
-
-  clearInputs();
-  closeModal();
-});
-
+addBtn.addEventListener("click", addNewBook);
 openModalBtn.addEventListener("click", openModal);
-
 closeModalBtn.addEventListener("click", closeModal);
